0870d54b0a57d7aa0a8680eacb3689b80cc4dbcc
NagiMuseibli <nagimuseibli02@gmail.com>
2024-05-10 12:00:43 +0400

    Checking database connetcion

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -1,1 +1,12 @@
 <?php
+
+
+$host = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "phpapi";
+
+$conn = mysqli_connect($host,$userName,$password,$dbName);
+if(!$conn){
+    die("Connection failed: " .mysqli_connect_error());
+}
